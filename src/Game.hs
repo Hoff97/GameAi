e00c38e44d57d974f54d35c60c550442dbf916b7
Hoff <frithjof97@web.de>
2015-10-07 13:53:21 +0200

    Minmax Alphabeta cutoff layout

--- src/Game.hs
+++ src/Game.hs
@@ -1,7 +1,8 @@
 module Game where
 
-import           Data.List (maximumBy, minimumBy)
-import           Data.Ord  (comparing)
+import           Control.Monad.State
+import           Data.List           (maximumBy, minimumBy)
+import           Data.Ord            (comparing)
 
 data Player = P1 | P2 deriving (Eq, Show)
 
@@ -21,3 +22,28 @@ maxPlay f p x a = a:maximumBy (comparing (f . last)) nxt
     where
         nxt = map (minPlay f nP (x-1)) $ next p a
         nP = if p == P1 then P2 else P1
+
+minD = -100000
+maxD = 100000
+
+alphaBeta :: Game a => Int -> (a -> Double) -> a -> [a] --TODO: Test
+alphaBeta d f a = fst $ minMax a f P1 d minD maxD
+
+minMax :: Game a => a -> (a -> Double) -> Player -> Int -> Double -> Double -> ([a],Double)
+minMax a f _ 0 _ _ = ([a],f a)
+minMax a f P1 i alpha beta = abPrune ([],minD) alpha beta (next P1 a)
+    where
+        abPrune s _ _ [] = s
+        abPrune r1@(_,h1) a b (x:xs) = if b<alphaNew then maxi else abPrune maxi alphaNew b xs
+            where
+                r2@(v2,h2) = minMax x f P2 (i-1) a b
+                maxi = if h1>h2 then r1 else r2
+                alphaNew = max a (snd maxi)
+minMax a f P2 i alpha beta = abPrune ([],maxD) alpha beta (next P2 a)
+    where
+        abPrune s _ _ [] = s
+        abPrune r1@(_,h1) a b (x:xs) = if betaNew<alpha then mini else abPrune mini a betaNew xs
+            where
+                r2@(v2,h2) = minMax x f P1 (i-1) a b
+                mini = if h1<h2 then r1 else r2
+                betaNew = min b (snd mini)
